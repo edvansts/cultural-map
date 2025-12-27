4027886dcddf1d5706a754f4e6d417e46cd2cc87
Edvan de Matos <edvan.santos@querodelivery.com>
2025-12-27 14:45:33 -0300

    feat: implement interactive map with user geolocation and markers

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,122 @@
-import Image from "next/image";
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
 
-export default function Home() {
+import { Map, Marker } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import {
+  AskPermissionResult,
+  useGeoLocation,
+} from "@/src/hooks/use-geolocation";
+import { useEffect, useState } from "react";
+import CasaDaVeraImage from "@/public/assets/arroz-com-galinha-da-vera.png";
+import { MapPoint } from "@/src/types/map";
+import { MapMarker } from "@/src/components/map-marker";
+import { FaLocationDot } from "react-icons/fa6";
+
+const interestPoints: MapPoint[] = [
+  {
+    id: 1,
+    name: "Casa de Vera do arroz de galinha",
+    mainImageUrl: CasaDaVeraImage,
+
+    location: {
+      latitude: -10.9126626,
+      longitude: -37.6743148,
+    },
+
+    phoneNumber: "(79) 99999-9999",
+  },
+];
+
+export default function MapPage() {
+  const { askForPermission, getCurrentPosition } = useGeoLocation();
+
+  const [permissionState, setPermissionState] = useState<AskPermissionResult>();
+
+  const [userGeolocation, setUserGeolocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>();
+
+  async function getPermissionState() {
+    const permissionState = await askForPermission();
+    setPermissionState(permissionState);
+  }
+
+  async function getCurrentPositionViewState() {
+    if (!permissionState) {
+      return;
+    }
+
+    if (permissionState.currentPosition) {
+      return setUserGeolocation({
+        latitude: permissionState.currentPosition.coords.latitude,
+        longitude: permissionState.currentPosition.coords.longitude,
+      });
+    }
+
+    if (permissionState.state !== "granted") {
+      return setUserGeolocation({
+        latitude: -10.0771918,
+        longitude: -37.3879654,
+      });
+    }
+
+    const position = await getCurrentPosition();
+
+    setUserGeolocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  useEffect(() => {
+    getPermissionState();
+  }, []);
+
+  useEffect(() => {
+    if (permissionState) {
+      getCurrentPositionViewState();
+    }
+  }, [permissionState]);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="flex flex-col min-h-screen items-center min-w-screen justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-semibold text-gray-800 bg-blue500 dark:text-zinc-50 absolute top-8 z-10">
+        Mapa Cultural de Lagarto-SE
+      </h1>
+
+      {userGeolocation ? (
+        <Map
+          initialViewState={{
+            ...userGeolocation,
+            zoom: 15,
+          }}
+          style={{ flex: 1 }}
+          mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${process.env.NEXT_PUBLIC_MAPBOX_API_KEY}`}
+        >
+          {interestPoints.map((point) => (
+            <MapMarker
+              key={point.id}
+              mapPoint={point}
+              userLocation={userGeolocation}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          ))}
+
+          <Marker
+            latitude={userGeolocation.latitude}
+            longitude={userGeolocation.longitude}
+            anchor="bottom"
           >
-            Documentation
-          </a>
-        </div>
-      </main>
+            <FaLocationDot color="#2b7fff" size={40} />
+            <span className="text-sm text-center font-medium ml-[-10] ">
+              Local atual
+            </span>
+          </Marker>
+        </Map>
+      ) : (
+        <div>Loading map...</div>
+      )}
     </div>
   );
 }
